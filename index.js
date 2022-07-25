4a27581f81d329960e0941128efff97f0b04699b
Muhammad Rizwan Ashiq <mrizwanashiq@outlook.com>
2022-07-25 06:27:29 +0000

    This is exmaple code for modular routing

--- index.js
+++ index.js
@@ -1,103 +1,17 @@
-// Importing package
 import express from 'express';
-import bodyParser from 'body-parser';
-import cors from 'cors';
+import students from './routes/students.js';
+import teachers from './routes/teachers.js';
+import courses from './routes/courses.js';
 
-const app = express();
-const port = 3000;
+const app = express()
 
-// Where we will keep books
-let books = [];
+app.use('/students', students); 
+// '/students' is the prefix for the students routes
 
-// Configuring middlewares
-app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use('/teachers', teachers); 
+// '/teachers' is the prefix for the teachers routes
 
-// POST API
-app.post('/book', (req, res) => {
-  const book = req.body;
+app.use('/courses', courses);   
+// '/courses' is the prefix for the courses routes
 
-  // here length of books' array will be the id of that book
-  book.id = books.length;
-
-  // Output the book to the console for debugging
-  console.log(book);
-  books.push(book);
-
-  res.send('Book is added to the database');
-});
-
-// GET API
-app.get('/books', (req, res) => {
-  res.json(books);
-});
-
-// GET by ID API
-app.get('/book/:id', (req, res) => {
-  // Reading ID from the URL
-  const id = req.params.id;
-
-  // Searching books for the id
-  for (let book of books) {
-      if (book.id === id) {
-          res.json(book);
-          return;
-      }
-  }
-
-  // Sending 404 when not found something is a good practice
-  res.status(404).send('Book not found');
-});
-
-// PUT API
-app.put('/book/:id', (req, res) => {
-  // Reading id from the URL
-  const id = req.params.id;
-  const book = req.body;
-
-  // Remove item from the books array
-  for (let i = 0; i < books.length; i++) {
-      let book = books[i]
-      if (book.id === id) {
-          books[i] = book;
-      }
-  }
-
-  res.send('Book is replace');
-});
-
-// PATCH API
-app.patch('/book/:id', (req, res) => {
-  // Reading id from the URL
-  const id = req.params.id;
-  const book = req.body;
-
-  // Remove item from the books array
-  for (let i = 0; i < books.length; i++) {
-      let book = books[i]
-      if (book.id === id) {
-          books[i] = {...book[i], ...book};
-      }
-  }
-
-  res.send('Book is update');
-});
-
-// DELETE API
-app.delete('/book/:id', (req, res) => {
-  // Reading id from the URL
-  const id = req.params.id;
-
-  // Remove item from the books array
-  books = books.filter(i => {
-      if (i.id !== id) {
-          return true;
-      }
-      return false;
-  });
-
-  res.send('Book is deleted');
-});
-
-app.listen(port, () => console.log(`Hello world app listening on port ${port}!`));
+app.listen(3000);
---
+++ routes/courses.js
@@ -0,0 +1,28 @@
+import express from 'express';
+const router = express.Router()
+
+let courses = []
+
+router.get('/', (req, res) => {
+    res.send(courses)
+})
+
+router.post('/', (req, res) => {
+    courses.push(req.body)
+    res.send(courses)
+})
+
+router.put('/:id', (req, res) => {
+    const id = req.params.id
+    const student = courses.find(student => student.id === id)
+    student.name = req.body.name
+    res.send(courses)
+})
+
+router.delete('/:id', (req, res) => {
+    const id = req.params.id
+    courses = courses.filter(student => student.id !== id)
+    res.send(courses)
+})
+
+export default router;
