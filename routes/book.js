3955b7111fb3622a0dba35267d8c0701df771a5f
mrizwanashiq <mrizwanashiq@outlook.com>
2022-07-27 22:35:10 +0500

    Fixed formatting

--- routes/book.js
+++ routes/book.js
@@ -3,28 +3,48 @@ const router = express.Router();
 import book from "../models/book.js";
 
 router.get("/", async (req, res) => {
-    const result = await book.find();
-    res.status(200).json(result);
+    try {
+        const books = await book.find();
+        res.json(books);
+    } catch (err) {
+        res.json({ message: err });
+    }
 })
 
 router.get("/:id", async (req, res) => {
-    const result = await book.findById(req.params.id);
-    res.status(200).json(result);
+    try {
+        const book = await book.findById(req.params.id);
+        res.json(book);
+    } catch (err) {
+        res.json({ message: err });
+    }
 })
 
 router.post("/", async (req, res) => {
-    const result = await book.create(req.body);
-    res.status(200).json(result);
+    try {
+        const result = await book.create(req.body);
+        res.status(200).json(result);
+    } catch (err) {
+        res.json({ message: err });
+    }
 })
 
 router.patch("/:id", async (req, res) => {
-    const result = await book.findByIdAndUpdate(req.params.id, req.body);
-    res.status(200).json(result);
+    try {
+        const result = await book.findByIdAndUpdate(req.params.id, req.body);
+        res.status(200).json(result);
+    } catch (err) {
+        res.json({ message: err });
+    }
 })
 
 router.delete("/:id", async (req, res) => {
-    const result = await book.findByIdAndDelete(req.params.id);
-    res.status(200).json(result);
+    try {
+        const result = await book.findByIdAndDelete(req.params.id);
+        res.status(200).json(result);
+    } catch (err) {
+        res.json({ message: err });
+    }
 })
 
 export default router;
