f969659ab2afbf2e2c627c25aa33b7b4f8713f34
Muhammad Rizwan Ashiq <mrizwanashiq@outlook.com>
2022-07-27 10:12:52 +0500

    Update book.js

--- routes/book.js
+++ routes/book.js
@@ -7,8 +7,23 @@ router.get("/", async (req, res) => {
     res.status(200).json(result);
 })
 
+router.get("/:id", async (req, res) => {
+    const result = await book.findById(req.params.id);
+    res.status(200).json(result);
+})
+
 router.post("/", async (req, res) => {
     const result = await book.create(req.body);
+    res.status(200).json(result);
+})
+
+router.patch("/:id", async (req, res) => {
+    const result = await book.findByIdAndUpdate(req.params.id, req.body);
+    res.status(200).json(result);
+})
+
+router.delete("/:id", async (req, res) => {
+    const result = await book.findByIdAndDelete(req.params.id);
     res.status(200).json(result);
 })
 
