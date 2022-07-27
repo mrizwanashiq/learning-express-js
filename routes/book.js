762de2f5201611e75f33b232449854c87801fab8
mrizwanashiq <mrizwanashiq@outlook.com>
2022-07-27 23:04:51 +0500

    Added try catch

--- routes/book.js
+++ routes/book.js
@@ -3,6 +3,18 @@ const router = express.Router();
 import book from "../models/book.js";
 
 router.get("/", async (req, res) => {
+    /**
+     * I am using try catch block to handle the error
+     * if there is any error in the code
+     * it will be handled by the catch block
+     * and the error will be return via the response
+     * and the user will be notified
+     * 
+     * And I will use try catch block everywhere in the code
+     * where there is a possibility of error, because it is a good practice
+     * I don't want to crash the whole application if there is an error
+     * I want to handle the error and return the user a response
+     */
     try {
         const books = await book.find();
         res.json(books);
