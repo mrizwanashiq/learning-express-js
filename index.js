fed905d2efb24e39ac679acf2a93228547716a57
mrizwanashiq <mrizwanashiq@outlook.com>
2022-07-27 22:22:05 +0500

    Fixed formatting

--- index.js
+++ index.js
@@ -4,101 +4,106 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 
 const app = express();
-const port = 3000;
+const port = 2022;
 
 // Where we will keep books
 let books = [];
 
 // Configuring middlewares
 app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+	extended: false
+}));
 app.use(bodyParser.json());
 
 // POST API
 app.post('/book', (req, res) => {
-  const book = req.body;
+	const book = req.body;
 
-  // here length of books' array will be the id of that book
-  book.id = books.length;
+	// here length of books' array will be the id of that book
+	book.id = books.length + 1;
 
-  // Output the book to the console for debugging
-  console.log(book);
-  books.push(book);
+	// Output the book to the console for debugging
+	console.log(book);
+	books.push(book);
 
-  res.send('Book is added to the database');
+	res.send('Book is added to the database');
 });
 
 // GET API
 app.get('/books', (req, res) => {
-  res.json(books);
+	res.json(books);
 });
 
 // GET by ID API
 app.get('/book/:id', (req, res) => {
-  // Reading ID from the URL
-  const id = parseInt(req.params.id);
+	// Reading ID from the URL
+	const id = parseInt(req.params.id);
 
-  // Searching books for the id
-  for (let book of books) {
-      if (book.id === id) {
-          res.json(book);
-          return;
-      }
-  }
+	// Searching books for the id
+	for (let book of books) {
+		if (book.id === id) {
+			res.json(book);
+			return;
+		}
+	}
 
-  // Sending 404 when not found something is a good practice
-  res.status(404).send('Book not found');
+	// Sending 404 when not found something is a good practice
+	res.status(404).send('Book not found');
 });
 
 // PUT API
 app.put('/book/:id', (req, res) => {
-  // Reading id from the URL
-  const id = parseInt(req.params.id);
-  const book = req.body;
-  book.id = id;
+	// Reading id from the URL
+	const id = parseInt(req.params.id);
+	const book = req.body;
+	book.id = id;
 
-  // Remove item from the books array
-  for (let i = 0; i < books.length; i++) {
-      let book = books[i]
-      if (book.id === id) {
-          books[i] = book;
-      }
-  }
+	// Remove item from the books array
+	for (let i = 0; i < books.length; i++) {
+		let book = books[i]
+		if (book.id === id) {
+			books[i] = book;
+		}
+	}
 
-  res.send('Book is replace');
+	res.send('Book is replace');
 });
 
 // PATCH API
 app.patch('/book/:id', (req, res) => {
-  // Reading id from the URL
-  const id = parseInt(req.params.id);
-  const book = req.body;
-  book.id = id;
+	// Reading id from the URL
+	const id = parseInt(req.params.id);
+	const book = req.body;
+	book.id = id;
 
-  // Remove item from the books array
-  for (let i = 0; i < books.length; i++) {
-      if (book[i].id === id) {
-          books[i] = {...book[i], ...book};
-      }
-  }
+	// Remove item from the books array
+	for (let i = 0; i < books.length; i++) {
+		if (book[i].id === id) {
+			books[i] = {
+				...book[i],
+				...book
+			};
+		}
+	}
 
-  res.send('Book is update');
+	res.send('Book is update');
 });
 
 // DELETE API
 app.delete('/book/:id', (req, res) => {
-  // Reading id from the URL
-  const id = parseInt(req.params.id);
+	// Reading id from the URL
+	const id = parseInt(req.params.id);
 
-  // Remove item from the books array
-  books = books.filter(i => {
-      if (i.id !== id) {
-          return true;
-      }
-      return false;
-  });
+	// Remove item from the books array
+	books = books.filter(i => {
+		if (i.id !== id) {
+			return true;
+		}
+		return false;
+	});
 
-  res.send('Book is deleted');
+	res.send('Book is deleted');
 });
 
 app.listen(port, () => console.log(`Hello world app listening on port ${port}!`));
