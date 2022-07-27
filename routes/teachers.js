ed7f08ea2aa4cb39bf55d84e517db66458123c64
Muhammad Rizwan Ashiq <mrizwanashiq@outlook.com>
2022-07-27 10:52:46 +0500

    Update teachers.js

--- routes/teachers.js
+++ routes/teachers.js
@@ -14,14 +14,14 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
     const id = req.params.id
-    const student = teachers.find(student => student.id === id)
+    const student = teachers.find(student => student.id == id)
     student.name = req.body.name
     res.send(teachers)
 })
 
 router.delete('/:id', (req, res) => {
     const id = req.params.id
-    teachers = teachers.filter(student => student.id !== id)
+    teachers = teachers.filter(student => student.id != id)
     res.send(teachers)
 })
 
