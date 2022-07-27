a0cf38d8e063b974054d5a59233b69b569cb6db5
Muhammad Rizwan Ashiq <mrizwanashiq@outlook.com>
2022-07-27 10:51:55 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
 import students from './routes/students.js';
 import teachers from './routes/teachers.js';
 import courses from './routes/courses.js';
 
 const app = express()
+
+// Configuring middlewares
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use('/students', students); 
 // '/students' is the prefix for the students routes
