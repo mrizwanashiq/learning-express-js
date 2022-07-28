65707a1ff100e54b695c48712144d02d95177fa0
mrizwanashiq <mrizwanashiq@outlook.com>
2022-07-28 11:18:38 +0500

    First commit

---
+++ models/user.js
@@ -0,0 +1,8 @@
+import mongoose from "mongoose";
+const schema = mongoose.Schema({
+  name: { type: String, required: true,},
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  age: { type: String },
+});
+export default mongoose.model("User", schema);
