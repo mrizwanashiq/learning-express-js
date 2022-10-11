37cd776e3bd696cc799343506bff0d1a38144c36
mrizwanashiq <mrizwanashiq@outlook.com>
2022-10-11 11:07:39 +0500

    Making it better

--- index.js
+++ index.js
@@ -1,22 +1,6 @@
-import express from "express";
-import bodyparser from "body-parser";
-import cors from "cors";
-import mongoose from "mongoose"
-import userRouter from "./routes/user.js";
+import { mongooseLoader, startServer } from "./loaders";
 
-const app = express();
-app.use(bodyparser.json());
-app.use(bodyparser.urlencoded({ extended: true }));
-app.use(cors());
-
-const connection = mongoose.connection
-connection.once("connected", () => console.log("Database Connected ~"))
-connection.on("error", error => console.log("Database Error: ", error))
-mongoose.connect("mongodb://localhost:27017/express-jwt", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-
-app.use("/user", userRouter);
+const app = startServer();
+mongooseLoader('mongodb://localhost:27017/db')
 
 app.listen(3000, () => console.log("Server Started ~"));
---
+++ loaders/index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import bodyparser from "body-parser";
+import cors from "cors";
+import mongoose from "mongoose"
+import userRouter from "../routes/user.js";
+
+export const startServer = () => {
+    const app = express();
+    app.use(bodyparser.json());
+    app.use(bodyparser.urlencoded({ extended: true }));
+    app.use(cors());
+    app.use("/user", userRouter);
+    return app
+}
+
+export const mongooseLoader = async (url) => {
+    const connection = mongoose.connection
+    connection.once("connected", () => console.log("Database Connected ~"))
+    connection.on("error", error => console.log("Database Error: ", error))
+    mongoose.connect(url, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+}
+export const mongooseUnloader = async () => {
+    await mongoose.disconnect()
+    console.log("Database Disconnected ~")
+}
