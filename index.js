7a0173b7c842eae860944e5a787c80f12ce29c6b
mrizwanashiq <mrizwanashiq@outlook.com>
2022-10-19 14:46:34 +0500

    making it better

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
-import { mongooseLoader, startServer } from "./loaders";
+import express from "express";
+import bodyparser from "body-parser";
+import cors from "cors";
+import mongoose from "mongoose"
+import userRouter from "./routes/user.js";
 
-const app = startServer();
-mongooseLoader('mongodb://localhost:27017/db')
+const app = express();
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use("/public", express.static("public"));
+app.use(cors());
+app.use("/user", userRouter);
 
-app.listen(3000, () => console.log("Server Started ~"));
+const connection = mongoose.connection
+connection.once("connected", () => console.log("Database Connected ~"))
+connection.on("error", error => console.log("Database Error: ", error))
+mongoose.connect('mongodb://localhost:27017/db', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+
+app.listen(2022, () => console.log("Server Started ~"));
--- models/user.js
+++ models/user.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 const schema = mongoose.Schema({
   name: { type: String, required: true, },
   email: { type: String, required: true },
+  image: { type: String },
   password: { type: String, required: true },
 });
 export default mongoose.model("User", schema);
