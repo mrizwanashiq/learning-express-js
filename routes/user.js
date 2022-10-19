8c461f58a5a2d78cde26015181e9a6eeda75c2f0
mrizwanashiq <mrizwanashiq@outlook.com>
2022-10-19 14:46:34 +0500

    making it better

--- routes/user.js
+++ routes/user.js
@@ -1,57 +1,38 @@
 import express from "express";
 const router = express.Router();
-import authenticate from "../middlewares/authenticate.js";
-import validation from "../middlewares/validation.js";
-import schema from '../validations/user.validations.js'
 import Service from "../services/user.js"
+import upload from '../middlewares/upload.js'
 
-router.get("/", authenticate, async (req, res) => {
+router.get("/", async (req, res) => {
     try {
-        const data = await Service.get();
+        const data = await Service.getAll();
         res.status(200).json(data);
     } catch (error) {
         res.status(500).send(error);
     }
 })
 
-router.get("/:id", authenticate, async (req, res) => {
+router.post("/", upload.array('image'), async (req, res) => {
     try {
-        const data = await Service.getById(req.params.id);
+        if (req.files)
+            req.body.image = `${req.protocol}://${req.get("host")}/${req.files[0].path}`;
+        const data = await Service.add(req.body);
+        res.status(200).json(data);
+    } catch (error) {
+        res.send(error);
+    }
+})
+
+router.patch("/:id", async (req, res) => {
+    try {
+        const data = await Service.update({ ...req.params, ...req.body });
         res.status(200).json(data);
     } catch (error) {
         res.status(500).send(error);
     }
 })
 
-router.post("/register", validation(schema.register.body, 'body'), async (req, res) => {
-    // try {
-    const data = await Service.register(req.body);
-    res.status(200).json(data);
-    // } catch (error) {
-    //     // res.status(error.status).send(error.message);
-    //     res.send(error);
-    // }
-})
-
-router.post("/login", validation(schema.login.body, 'body'), async (req, res) => {
-    try {
-        const data = await Service.login(req.body);
-        res.status(200).json(data);
-    } catch (error) {
-        res.status(error.status).send(message);
-    }
-})
-
-router.patch("/:id", validation(schema.update.body, 'body'), authenticate, async (req, res) => {
-    try {
-        const user = await userModel.findByIdAndUpdate({ ...req.params, ...req.body });
-        res.status(200).json(data);
-    } catch (error) {
-        res.status(500).send(error);
-    }
-})
-
-router.delete("/:id", authenticate, async (req, res) => {
+router.delete("/:id", async (req, res) => {
     try {
         const data = await Service.delete(req.params.id);
         res.status(200).json(data);
@@ -61,31 +42,3 @@ router.delete("/:id", authenticate, async (req, res) => {
 })
 
 export default router;
-
-import express from 'express'
-import bodyParser from 'body-parser'
-import multer from 'multer'
-
-const app = express()
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
-
-// parse application/json
-app.use(bodyParser.json())
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, './uploads')
-    },
-    filename: (req, file, cb) => {
-        cb(null, file.originalname)
-    }
-})
-
-const upload = multer({ storage })
-app.post('/', upload.single('foo'), (req, res) => {
-    const a = 10;
-    res.send('done')
-
-})
-
-app.listen(2022)
--- services/user.js
+++ services/user.js
@@ -1,5 +1,4 @@
 import UserModel from "../models/user.js"
-import jwt from 'jsonwebtoken'
 
 const UserService = {
     getAll: async () => {
@@ -10,25 +9,9 @@ const UserService = {
         const data = await UserModel.findById(id);
         return { message: "success", data }
     },
-    register: async (body) => {
-        const isExists = await UserModel.findOne({ email: body.email })
-
-        if (isExists) {
-            throw ({ status: 401, message: "User already exists" })
-        }
-
+    add: async (body) => {
         const data = await UserModel.create(body);
         return { message: "success", data }
-    },
-    login: async (body) => {
-        const isExists = await UserModel.findOne({ email: body.email, password: body.password })
-
-        if (!isExists) {
-            throw ({ status: 404, message: "User not found with this email and password" })
-        }
-        const data = await UserModel.create(body);
-        const token = jwt.sign(data._doc, "my_secret",)
-        return { message: "success", token }
     },
     update: async ({ id, ...body }) => {
         const data = await UserModel.findByIdAndUpdate(id, body);
