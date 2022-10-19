e38632ce18d306e8b27600bc5c8ecbdec64129fc
mrizwanashiq <mrizwanashiq@outlook.com>
2022-10-19 10:00:57 +0500

    Making it better

--- routes/user.js
+++ routes/user.js
@@ -61,3 +61,31 @@ router.delete("/:id", authenticate, async (req, res) => {
 })
 
 export default router;
+
+import express from 'express'
+import bodyParser from 'body-parser'
+import multer from 'multer'
+
+const app = express()
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, './uploads')
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname)
+    }
+})
+
+const upload = multer({ storage })
+app.post('/', upload.single('foo'), (req, res) => {
+    const a = 10;
+    res.send('done')
+
+})
+
+app.listen(2022)
