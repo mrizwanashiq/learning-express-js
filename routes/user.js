2f7ebdb873ce5823b647a4ed7368869b802c5359
mrizwanashiq <mrizwanashiq@outlook.com>
2022-11-08 08:21:36 +0500

    making it better

--- routes/user.js
+++ routes/user.js
@@ -12,10 +12,10 @@ router.get("/", async (req, res) => {
     }
 })
 
-router.post("/", upload.array('image'), async (req, res) => {
+router.post("/", upload.single('image'), async (req, res) => {
     try {
-        if (req.files)
-            req.body.image = `${req.protocol}://${req.get("host")}/${req.files[0].path}`;
+        if (req.file)
+            req.body.image = `${req.protocol}://${req.get("host")}/${req.file.path}`;
         const data = await Service.add(req.body);
         res.status(200).json(data);
     } catch (error) {
