ebe6a4dbb485d395bc813e07ddb1a22a5e56b7c3
Muhammad Rizwan Ashiq <mrizwanashiq@outlook.com>
2022-12-18 23:42:11 -0800

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(cors());
 const connection = mongoose.connection
 connection.once("connected", () => console.log("Database Connected ~"))
 connection.on("error", error => console.log("Database Error: ", error))
-mongoose.connect("mongodb://localhost:27017/my_first_data_base", {
+mongoose.connect("mongodb://127.0.0.1:27017/my_first_data_base", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
