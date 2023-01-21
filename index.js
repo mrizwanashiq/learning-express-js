b955f14471fa3a1485ae29ebadd0152a2b96dc73
mrizwanashiq <mrizwanashiq@outlook.com>
2023-01-21 12:17:57 +0500

    Enviroment variables

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import express from "express";
 import bodyparser from "body-parser";
 import cors from "cors";
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 import userRouter from "./routes/user.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 app.use(bodyparser.json());
@@ -11,12 +14,12 @@ app.use("/public", express.static("public"));
 app.use(cors());
 app.use("/user", userRouter);
 
-const connection = mongoose.connection
-connection.once("connected", () => console.log("Database Connected ~"))
-connection.on("error", error => console.log("Database Error: ", error))
-mongoose.connect('mongodb://localhost:27017/db', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
+const connection = mongoose.connection;
+connection.once("connected", () => console.log("Database Connected ~"));
+connection.on("error", (error) => console.log("Database Error: ", error));
+mongoose.connect(process.env.MONGO_URI, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
 
-app.listen(2022, () => console.log("Server Started ~"));
+app.listen(3000, () => console.log("Server Started ~"));
