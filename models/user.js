42316585a4972823e0481ba5133111cbeb68ecec
mrizwanashiq <mrizwanashiq@outlook.com>
2023-02-08 23:35:35 +0500

    FEAT

--- models/user.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const schema = mongoose.Schema({
-  name: { type: String, required: true,},
-  email: { type: String, required: true },
-  password: { type: String, required: true },
-  age: { type: String },
-});
-module.exports = mongoose.model("User", schema);
