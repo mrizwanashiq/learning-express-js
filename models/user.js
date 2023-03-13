48518fcbc92b67e57c41822d822092aaaca54795
mrizwanashiq <mrizwanashiq@outlook.com>
2023-03-13 21:06:45 +0500

    feat

--- models/user.js
+++ models/user.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
 const schema = mongoose.Schema({
-  name: { type: String, required: true,},
-  email: { type: String, required: true },
-  password: { type: String, required: true },
-  age: { type: String },
+	name: { type: String, required: true },
+	email: { type: String, required: true },
+	password: { type: String, required: true },
+	age: { type: String },
 });
 export default mongoose.model("User", schema);
