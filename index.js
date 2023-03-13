c6ce18ed9a35addd9a1bd78dfe04a2be8dc2a823
mrizwanashiq <mrizwanashiq@outlook.com>
2023-03-13 21:03:57 +0500

    feat

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
 import express from "express";
-import bodyparser from "body-parser";
-import cors from "cors";
-import mongoose from "mongoose"
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
 import userRouter from "./routes/user.js";
 
 const app = express();
-app.use(bodyparser.json());
-app.use(bodyparser.urlencoded({ extended: true }));
-app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-const connection = mongoose.connection
-connection.once("connected", () => console.log("Database Connected ~"))
-connection.on("error", error => console.log("Database Error: ", error))
+const connection = mongoose.connection;
+connection.once("connected", () => console.log("Database Connected ~"));
+connection.on("error", (error) => console.log("Database Error: ", error));
 mongoose.connect("mongodb://localhost:27017/express-jwt", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
 
 app.use("/user", userRouter);
 
