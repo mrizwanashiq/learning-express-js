fe3668e67bddaaad48dd9b9f255d5d7f923c70a1
mrizwanashiq <mrizwanashiq@outlook.com>
2023-03-13 21:02:08 +0500

    feat\

--- models/user.js
+++ models/user.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
 const schema = mongoose.Schema({
-  name: { type: String, required: true, },
-  email: { type: String, required: true },
-  image: { type: String },
-  password: { type: String, required: true },
+	name: { type: String, required: true },
+	email: { type: String, required: true },
+	image: { type: String },
+	password: { type: String, required: true },
 });
 export default mongoose.model("User", schema);
--- services/user.js
+++ services/user.js
@@ -1,26 +1,26 @@
-import UserModel from "../models/user.js"
+import UserModel from "../models/user.js";
 
 const UserService = {
-    getAll: async () => {
-        const data = await UserModel.find();
-        return { message: "success", data }
-    },
-    getById: async (id) => {
-        const data = await UserModel.findById(id);
-        return { message: "success", data }
-    },
-    add: async (body) => {
-        const data = await UserModel.create(body);
-        return { message: "success", data }
-    },
-    update: async ({ id, ...body }) => {
-        const data = await UserModel.findByIdAndUpdate(id, body);
-        return { message: "success", data }
-    },
-    delete: async (id) => {
-        const data = await UserModel.findByIdAndRemove(id);
-        return { message: "success", data }
-    }
-}
+	getAll: async () => {
+		const data = await UserModel.find();
+		return { message: "success", data };
+	},
+	getById: async (id) => {
+		const data = await UserModel.findById(id);
+		return { message: "success", data };
+	},
+	add: async (body) => {
+		const data = await UserModel.create(body);
+		return { message: "success", data };
+	},
+	update: async ({ id, ...body }) => {
+		const data = await UserModel.findByIdAndUpdate(id, body);
+		return { message: "success", data };
+	},
+	delete: async (id) => {
+		const data = await UserModel.findByIdAndRemove(id);
+		return { message: "success", data };
+	},
+};
 
 export default UserService;
