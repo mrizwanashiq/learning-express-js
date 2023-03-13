33207be1983008f66662abd1fd3199a238329684
mrizwanashiq <mrizwanashiq@outlook.com>
2023-03-13 21:03:57 +0500

    feat

--- routes/user.js
+++ routes/user.js
@@ -7,59 +7,74 @@ import validate from "../middlewares/validate.js";
 import schema from "../validations/user.validation.js";
 
 router.get("/", authenticate, async (req, res) => {
-    try {
-        const users = await userModel.find();
-        res.send(users);
-    } catch (error) {
-        res.status(500).send(error);
-    }
-})
+	try {
+		const users = await userModel.find();
+		res.send(users);
+	} catch (error) {
+		res.status(500).send(error);
+	}
+});
 
 router.get("/:id", authenticate, validate(schema.params), async (req, res) => {
-    try {
-        const user = await userModel.findById(req.params.id);
-        res.send(user);
-    } catch (error) {
-        res.status(500).send(error);
-    }
-})
+	try {
+		const user = await userModel.findById(req.params.id);
+		res.send(user);
+	} catch (error) {
+		res.status(500).send(error);
+	}
+});
 
 router.post("/register", validate(schema.register), async (req, res) => {
-    try {
-        const user = await userModel.create(req.body);
-        res.send(user);
-    } catch (error) {
-        res.status(500).send(error);
-    }
-})
+	try {
+		const user = await userModel.create(req.body);
+		res.send(user);
+	} catch (error) {
+		res.status(500).send(error);
+	}
+});
 
 router.post("/login", validate(schema.login), async (req, res) => {
-    try {
-        const user = await userModel.findOne({ email: req.body.email, password: req.body.password });
-        if (!user) return res.status(401).send("Invalid email or password.");
-        const token = jwt.sign({ id: user._id }, "my_temporary_secret", { expiresIn: "1h" });
-        res.send(token);
-    } catch (error) {
-        res.status(500).send(error);
-    }
-})
+	try {
+		const user = await userModel.findOne({
+			email: req.body.email,
+			password: req.body.password,
+		});
+		if (!user) return res.status(401).send("Invalid email or password.");
+		const token = jwt.sign({ id: user._id }, "my_temporary_secret", {
+			expiresIn: "1h",
+		});
+		res.send(token);
+	} catch (error) {
+		res.status(500).send(error);
+	}
+});
 
-router.patch("/:id", authenticate, validate(schema.update), async (req, res) => {
-    try {
-        const user = await userModel.findByIdAndUpdate(req.params.id, req.body);
-        res.send(user);
-    } catch (error) {
-        res.status(500).send(error);
-    }
-})
+router.patch(
+	"/:id",
+	authenticate,
+	validate(schema.update),
+	async (req, res) => {
+		try {
+			const user = await userModel.findByIdAndUpdate(req.params.id, req.body);
+			res.send(user);
+		} catch (error) {
+			res.status(500).send(error);
+		}
+	}
+);
 
-router.delete("/:id", authenticate, validate(schema.params), async (req, res) => {
-    try {
-        const user = await userModel.findByIdAndDelete(req.params.id);
-        res.send(user);
-    } catch (error) {
-        res.status(500).send(error);
-    }
-})
+router.delete(
+	"/:id",
+	authenticate,
+	validate(schema.params),
+	async (req, res) => {
+		try {
+			const user = await userModel.findByIdAndDelete(req.params.id);
+			res.send(user);
+		} catch (error) {
+			res.status(500).send(error);
+		}
+	}
+);
 
 export default router;
