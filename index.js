fccd9d2f47e87f63485158646d2cf40c41ef0324
mrizwanashiq <mrizwanashiq@outlook.com>
2023-03-13 21:05:00 +0500

    feat

--- index.js
+++ index.js
@@ -1,24 +1,22 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import students from './routes/students.js';
-import teachers from './routes/teachers.js';
-import courses from './routes/courses.js';
+import express from "express";
+import bodyParser from "body-parser";
+import students from "./routes/students.js";
+import teachers from "./routes/teachers.js";
+import courses from "./routes/courses.js";
 
-const app = express()
+const app = express();
 
 // Configuring middlewares
-app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use('/students', students); 
+app.use("/students", students);
 // '/students' is the prefix for the students routes
 
-app.use('/teachers', teachers); 
+app.use("/teachers", teachers);
 // '/teachers' is the prefix for the teachers routes
 
-app.use('/courses', courses);   
+app.use("/courses", courses);
 // '/courses' is the prefix for the courses routes
 
 app.listen(3000);
