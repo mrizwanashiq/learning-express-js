3a4a7701ce4e9aa27e024fd7d8882430c520398b
mrizwanashiq <mrizwanashiq@outlook.com>
2023-03-13 20:58:48 +0500

    feat

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -1,21 +1,21 @@
-import multer from 'multer'
-import path from 'path'
+import multer from "multer";
+import path from "path";
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, "./public");
-    },
-    filename: (req, file, cb) => {
-        const fileName =
-            path
-                .basename(file.originalname, path.extname(file.originalname))
-                .toLowerCase()
-                .replace(/\s+/g, "-") +
-            "-" +
-            Date.now().toString() +
-            path.extname(file.originalname).toLowerCase();
-        cb(null, fileName);
-    },
+	destination: (req, file, cb) => {
+		cb(null, "./public");
+	},
+	filename: (req, file, cb) => {
+		const fileName =
+			path
+				.basename(file.originalname, path.extname(file.originalname))
+				.toLowerCase()
+				.replace(/\s+/g, "-") +
+			"-" +
+			Date.now().toString() +
+			path.extname(file.originalname).toLowerCase();
+		cb(null, fileName);
+	},
 });
 
-export default multer({ storage })
+export default multer({ storage });
