400f44d454b6e3abed2dafe3502e7837eb850753
mrizwanashiq <mrizwanashiq@outlook.com>
2023-03-18 08:06:40 +0500

    feat

--- routes/book.js
+++ routes/book.js
@@ -1,10 +1,70 @@
 import express from "express";
 const router = express.Router();
-import book from "../models/book.js";
+import bookModel from "../models/book.js";
 
 router.get("/", async (req, res) => {
 	try {
-		const books = await book.find();
+		// http://localhost:3000/book?author=J.K.Rowling&price[gt]=1000&stock[lt]=10&name=Harry Potter&id=5f9f1c1b0b1b8c1e1c8c1c1c&sort=price&sort=-name
+		const query = {};
+		if (req.query._id) {
+			query._id = req.query._id;
+		}
+		if (req.query.name) {
+			query.name = req.query.name;
+		}
+		if (req.query.author) {
+			query.author = req.query.author;
+		}
+		if (req.query.price) {
+			if(typeof req.query.price === 'object'){
+				if (req.query.price.gt) {
+					query.price = { $gt: req.query.price.gt };
+				}
+				if (req.query.price.lt) {
+					query.price = { $lt: req.query.price.lt };
+				}
+			}else{
+				query.price = req.query.price;
+			}
+		}
+
+		if (req.query.stock) {
+			if(typeof req.query.stock === 'object'){
+				if (req.query.stock.gt) {
+					query.stock = { $gt: req.query.stock.gt };
+				}
+				if (req.query.stock.lt) {
+					query.stock = { $lt: req.query.stock.lt };
+				}
+
+			}else{
+				query.stock = req.query.stock;
+			}
+		}
+
+		const sort = {};
+		if(req.query.sort){
+			if(typeof req.query.sort === 'object'){
+				req.query.sort.forEach(element => {
+					if(element.startsWith('-')){
+						sort[element.substring(1)] = -1;
+					}else{
+						sort[element] = 1;
+					}
+				});
+				console.log(sort);
+			}else{
+				if(req.query.sort.startsWith('-')){
+					sort[req.query.sort.substring(1)] = -1;
+				}else{
+					sort[req.query.sort] = 1;
+				}
+				console.log(sort);
+			}
+		}
+
+		const books = await bookModel.find(query).sort(sort);
+
 		res.json(books);
 	} catch (err) {
 		res.json({ message: err });
@@ -22,7 +82,7 @@ router.get("/:id", async (req, res) => {
 
 router.post("/", async (req, res) => {
 	try {
-		const result = await book.create(req.body);
+		const result = await bookModel.create(req.body);
 		res.status(200).json(result);
 	} catch (err) {
 		res.json({ message: err });
@@ -31,7 +91,7 @@ router.post("/", async (req, res) => {
 
 router.patch("/:id", async (req, res) => {
 	try {
-		const result = await book.findByIdAndUpdate(req.params.id, req.body);
+		const result = await bookModel.findByIdAndUpdate(req.params.id, req.body);
 		res.status(200).json(result);
 	} catch (err) {
 		res.json({ message: err });
@@ -40,7 +100,7 @@ router.patch("/:id", async (req, res) => {
 
 router.delete("/:id", async (req, res) => {
 	try {
-		const result = await book.findByIdAndDelete(req.params.id);
+		const result = await bookModel.findByIdAndDelete(req.params.id);
 		res.status(200).json(result);
 	} catch (err) {
 		res.json({ message: err });
