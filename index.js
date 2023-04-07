9d9c15e1b8cd32afcb1ba9356c576f5d3fe1c08f
mrizwanashiq <mrizwanashiq@outlook.com>
2023-04-07 12:45:09 +0500

    feat

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 import express from "express";
-import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import fileRouter from "./routes/file.js";
 
 const app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use("/public", express.static("public"));
 app.use("/file", fileRouter);
 
