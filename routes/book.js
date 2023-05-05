8ed132a865a17ac2336d6c16eb82a4acb3254866
mrizwanashiq <mrizwanashiq@outlook.com>
2023-05-05 17:39:25 +0500

    feat

--- routes/book.js
+++ routes/book.js
@@ -1,50 +1,12 @@
 import express from "express";
+import BookController from "../controllers/book.js";
+
 const router = express.Router();
-import book from "../models/book.js";
 
-router.get("/", async (req, res) => {
-	try {
-		const books = await book.find();
-		res.json(books);
-	} catch (err) {
-		res.json({ message: err });
-	}
-});
-
-router.get("/:id", async (req, res) => {
-	try {
-		const book = await book.findById(req.params.id);
-		res.json(book);
-	} catch (err) {
-		res.json({ message: err });
-	}
-});
-
-router.post("/", async (req, res) => {
-	try {
-		const result = await book.create(req.body);
-		res.status(200).json(result);
-	} catch (err) {
-		res.json({ message: err });
-	}
-});
-
-router.patch("/:id", async (req, res) => {
-	try {
-		const result = await book.findByIdAndUpdate(req.params.id, req.body);
-		res.status(200).json(result);
-	} catch (err) {
-		res.json({ message: err });
-	}
-});
-
-router.delete("/:id", async (req, res) => {
-	try {
-		const result = await book.findByIdAndDelete(req.params.id);
-		res.status(200).json(result);
-	} catch (err) {
-		res.json({ message: err });
-	}
-});
+router.get("/", BookController.get);
+router.get("/:id", BookController.getById);
+router.post("/", BookController.create);
+router.patch("/:id", BookController.update);
+router.delete("/:id", BookController.delete);
 
 export default router;
