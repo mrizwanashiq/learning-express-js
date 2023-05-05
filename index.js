06877818b2b3dd6a388b96cb2c201d000e0c8c50
mrizwanashiq <mrizwanashiq@outlook.com>
2023-05-05 17:42:12 +0500

    feat

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const connection = mongoose.connection;
+connection.once("connected", () => console.log("Database Connected ~"));
+connection.on("error", (error) => console.log("Database Error: ", error));
 mongoose.connect("mongodb://127.0.0.1:27017/my_first_data_base", {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
 });
-const connection = mongoose.connection;
-connection.once("connected", () => console.log("Database Connected ~"));
-connection.on("error", (error) => console.log("Database Error: ", error));
 
 app.use("/book", bookRouter);
 
