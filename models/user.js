acbbf0403b3919a391d61f6df549d5ddbff21f3a
Muhammad Rizwan Ashiq <mrizwanashiq@outlook.com>
2023-06-27 01:15:13 +0500

    Update user.js

--- models/user.js
+++ models/user.js
@@ -3,6 +3,6 @@ const schema = mongoose.Schema({
   name: { type: String, required: true,},
   email: { type: String, required: true },
   password: { type: String, required: true },
-  age: { type: String },
+  age: { type: String, min: 18 },
 });
 export default mongoose.model("User", schema);
