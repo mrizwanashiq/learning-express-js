578e0dd98752ec222d6c6fbe8eb8168c19849d5b
mrizwanashiq <mrizwanashiq@outlook.com>
2023-06-27 10:20:09 +0500

    feat: changed the middleware's code

--- middlewares/validate.js
+++ middlewares/validate.js
@@ -1,29 +1,11 @@
-import Joi from "joi";
-
-const validate = (schema) => (req, res, next) => {
-	const validSchema = pick(schema, ["params", "query", "body"]);
-	const object = pick(req, Object.keys(validSchema));
-	const { value, error } = Joi.compile(validSchema)
-		.prefs({ errors: { label: "key" }, abortEarly: false })
-		.validate(object);
-
-	if (error) {
-		const errorMessage = error.details
-			.map((details) => details.message)
-			.join(", ");
-		return res.status(401).json(errorMessage);
-	}
-	Object.assign(req, value);
-	return next();
-};
-
-const pick = (object, keys) => {
-	return keys.reduce((obj, key) => {
-		if (object && Object.prototype.hasOwnProperty.call(object, key)) {
-			obj[key] = object[key];
-		}
-		return obj;
-	}, {});
+const validate = (schema) => {
+    return (req, res, next) => {
+        const { error } = schema.validate(req.body);
+        if (error) {
+            return res.status(400).send(error);
+        }
+        next();
+    };
 };
 
 export default validate;
