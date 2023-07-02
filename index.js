00c4e07ce3654ea6e9f8950d0b505759d5e4f3e7
mrizwanashiq <mrizwanashiq@outlook.com>
2023-07-02 19:39:39 +0500

    feat

--- index.js
+++
@@ -1,8 +0,0 @@
-import { mongooseLoader, startServer } from "./loaders";
-
-const app = startServer();
-mongooseLoader('mongodb://localhost:27017/db')
-
-// app.listen(3000, () => console.log("Server Started ~"));
-
-export default app
--- models/user.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-const schema = mongoose.Schema({
-  name: { type: String, required: true, },
-  email: { type: String, required: true },
-  age: { type: Number, required: true },
-});
-export default mongoose.model("User", schema);
