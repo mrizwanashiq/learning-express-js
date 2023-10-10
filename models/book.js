b9f6bedaa20c05bf8cd01c9608de8d2d6b4ae98e
mrizwanashiq <mrizwanashiq@outlook.com>
2023-10-10 10:45:08 +0500

    initial commit

--- models/book.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-const schema = mongoose.Schema({
-	name: { type: String, required: true },
-	author: { type: String, required: true },
-	price: { type: Number, required: true },
-	stock: { type: Number },
-});
-export default mongoose.model("Book", schema);
